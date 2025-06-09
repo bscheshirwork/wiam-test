dd3dc4e0a4b6ca7b3274e33badaa185c8ad96b02
dev <bscheshir.work@gmail.com>
2025-06-09 19:00:58 +0300

    Работа с openApi - cors

--- modules/api/controllers/BaseApiController.php
+++ modules/api/controllers/BaseApiController.php
@@ -3,6 +3,7 @@
 namespace app\modules\api\controllers;
 
 use yii\filters\ContentNegotiator;
+use yii\filters\Cors;
 use yii\filters\VerbFilter;
 use yii\rest\Controller;
 use yii\rest\OptionsAction;
@@ -10,29 +11,36 @@ use yii\web\Response;
 
 class BaseApiController extends Controller
 {
+    protected const BEHAVIOR_CONTENT_NEGOTIATION = 'contentNegotiation';
+    protected const BEHAVIOR_VERB_FILTER = 'verbFilter';
+    protected const BEHAVIOR_CORS_FILTER = 'corsFilter';
+
     public function behaviors(): array
     {
         return [
-            'contentNegotiator' => [
+            self::BEHAVIOR_CONTENT_NEGOTIATION => [
                 'class' => ContentNegotiator::class,
                 'formats' => [
                     'application/json' => Response::FORMAT_JSON,
                 ],
             ],
-            'verbFilter' => [
+            self::BEHAVIOR_VERB_FILTER => [
                 'class' => VerbFilter::class,
                 'actions' => $this->verbs(),
             ],
+            self::BEHAVIOR_CORS_FILTER => [
+                'class' => Cors::class,
+                'cors' => [
+                    'Origin' => [
+                        env('CORS_ORIGIN_SWAGGER', 'http://0.0.0.0:8001'),
+                    ], //responseHeaders Access-Control-Allow-Origin
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], //Access-Control-Allow-Methods
+                    'Access-Control-Request-Headers' => ['*'], // 'Access-Control-Allow-Headers'
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age' => 3600,
+                ],
+            ],
         ];
-    }
-
-    protected function getRequestData(): array
-    {
-        if ($this->request->isPost) {
-            return $this->request->post();
-        }
-
-        return $this->request->get();
     }
 
     public function actions(): array
@@ -42,5 +50,27 @@ class BaseApiController extends Controller
                 'class' => OptionsAction::class,
             ],
         ];
+    }
+
+    public function afterAction($action, $result): Response
+    {
+        /** @var Cors $cors */
+        $cors = $this->getBehavior(self::BEHAVIOR_CORS_FILTER);
+        if ($cors->response->headers->count()) {
+            foreach ($cors->response->headers as $name => $value) {
+                $result->headers->set($name, $value);
+            }
+        }
+
+        return $result;
+    }
+
+    protected function getRequestData(): array
+    {
+        if ($this->request->isPost) {
+            return $this->request->post();
+        }
+
+        return $this->request->get();
     }
 }
